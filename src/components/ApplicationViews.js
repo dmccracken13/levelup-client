a0d7a902406b3309a7dbdfd6b862e19150622070
Danny McCracken <dannymccracken13@yahoo.com>
2021-02-10 16:01:14 -0600

    implemented code for game form

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,10 +2,11 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { GameList } from "./game/GameList.js"
 import { GameProvider } from "./game/GameProvider.js"
+import { GameForm } from "./game/GameForm.js"
 import { EventProvider } from "./game/EventProvider.js"
 import { EventList } from "./game/EventList.js"
 
-export const ApplicationViews = () => {
+export const ApplicationViews = (props) => {
     return <>
         <main style={{
             margin: "5rem 2rem",
@@ -13,9 +14,15 @@ export const ApplicationViews = () => {
         }}> 
             <GameProvider>
                 <Route exact path="/">
-                    <GameList />
+                    <GameList {...props}/>
                 </Route>
             </GameProvider>
+
+            <GameProvider>   
+                <Route exact path="/games/new">
+                    <GameForm {...props}/>
+                </Route>
+            </GameProvider>    
 
             <EventProvider>
                 <Route exact path="/events">
